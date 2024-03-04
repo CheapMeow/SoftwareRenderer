cf0f55429585b143f7008010984dcdfee81b89e8
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2024-03-05 01:40:22 +0800

    Phong shading

--- src/app/scene.cpp
+++ src/app/scene.cpp
@@ -1,69 +1,66 @@
 #include "scene.h"
 #include "objParser.h"
 
-Scene::Scene(std::string path) { emptyScene = loadSceneModels(path); }
+Scene::Scene(std::string path){
+    emptyScene = loadSceneModels(path);
+}
 
-Scene::~Scene()
-{
-    // Making sure you don't attempt to delete models that don't exist
-    if (!emptyScene)
-    {
-        for (Model* models : modelsInScene)
-        {
+Scene::~Scene(){
+    //Making sure you don't attempt to delete models that don't exist
+    if (!emptyScene){
+        for(Model *models : modelsInScene){
             delete models;
         }
     }
 }
 
-void Scene::update()
-{
+void Scene::update(){
     mainCamera.update();
-    for (Model* models : modelsInScene)
-    {
+    for(Model *models : modelsInScene){
         models->update();
     }
     frustrumCulling();
 }
 
-bool Scene::loadSceneModels(std::string& path)
-{
-    // In the future I want to read all o the models in the model folder
-    // And build them here. For now only one is loaded.
-    std::string fullPath = "../../../resources/models/";
-    fullPath             = fullPath + path;
+bool Scene::loadSceneModels(std::string &path){
+    //In the future I want to read all o the models in the model folder
+    //And build them here. For now only one is loaded.
+    std::string fullPath = "../models/";
+    fullPath = fullPath + path;
 
-    if (!OBJ::fileExists(fullPath))
-    {
-        printf("Error! File:%s does not exist.\n", path.c_str());
+    if(!OBJ::fileExists(fullPath)){
+        printf("Error! File:%s does not exist.\n",path.c_str());
         return true;
     }
-    else
-    {
+    else{
         TransformParameters initParameters;
-        // initParameters.rotation = Vector3f(90*M_PI/180.0f, 0 , 0);
-        initParameters.translation = Vector3f(0, 0, 0);
+        //initParameters.rotation = Vector3f(90*M_PI/180.0f, 0 , 0);
+        initParameters.translation = Vector3f(0, -5, 0);
         modelsInScene.push_back(new Model(fullPath, initParameters));
         return false;
     }
 }
 
-void Scene::frustrumCulling()
-{
+void Scene::frustrumCulling(){
     bool visible = true;
-    for (Model* model : modelsInScene)
-    {
+    for(Model *model : modelsInScene){
 
         visible = mainCamera.checkVisibility(model->getBounds());
 
-        if (visible)
-        {
+        if (visible) {
             visibleModels.push(model);
         }
     }
 }
 
-std::queue<Model*>* Scene::getVisiblemodels() { return &visibleModels; }
+ std::queue<Model*>* Scene::getVisiblemodels(){
+    return &visibleModels;
+}
 
-Camera* Scene::getCurrentCamera() { return &mainCamera; }
+Camera* Scene::getCurrentCamera(){
+    return &mainCamera;
+}
 
-bool Scene::checkIfEmpty() { return emptyScene; }
+bool Scene::checkIfEmpty(){
+    return emptyScene;
+}
