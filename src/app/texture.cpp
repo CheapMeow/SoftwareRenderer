9d7b34f01a6b70af60c947030b4835bfb2851933
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2024-03-05 10:18:40 +0800

    perspective correct barycentric coords

--- src/app/texture.cpp
+++ src/app/texture.cpp
@@ -2,7 +2,9 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-Texture::Texture(std::string path) { 
+Texture::Texture(std::string path)
+{
+    stbi_set_flip_vertically_on_load(true);
     pixelData = stbi_load(path.c_str(), &width, &height, &channels, 0);
 }
 
@@ -10,7 +12,7 @@ Vector3f Texture::getPixelVal(float u, float v)
 {
     float intpart;
     int   uInt  = std::modf(u, &intpart) * (width - 1);
-    int   vInt  = (1 - std::modf(v, &intpart)) * (height - 1);
+    int   vInt  = std::modf(v, &intpart) * (height - 1);
     int   index = (vInt * width + uInt) * 3;
     // printf("%d, %d, %d, %x, %x, %x\n",vInt, uInt, index ,pixelData[index],pixelData[index+1],pixelData[index+2]);
     float val1 = pixelData[index];
