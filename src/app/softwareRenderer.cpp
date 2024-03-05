1fc0e927261d0a126bea3a903e7944f7d897403f
CheapMeow <blizzzzzzzzzzzzard@gmail.com>
2024-03-05 11:16:32 +0800

    add openmp

--- src/app/softwareRenderer.cpp
+++ src/app/softwareRenderer.cpp
@@ -1,127 +1,136 @@
 #include "softwareRenderer.h"
-#include "shader.h"
 #include "mesh.h"
+#include "omp.h"
+#include "shader.h"
 
-SoftwareRenderer::SoftwareRenderer(){}
-SoftwareRenderer::~SoftwareRenderer(){}
+SoftwareRenderer::SoftwareRenderer() {}
+SoftwareRenderer::~SoftwareRenderer() {}
 
-bool SoftwareRenderer::startUp(int w, int h){
-    if( !createBuffers(w, h) ){
+bool SoftwareRenderer::startUp(int w, int h)
+{
+    if (!createBuffers(w, h))
+    {
         return false;
     }
     startUpComplete = true;
     return true;
 }
 
-void SoftwareRenderer::shutDown(){
+void SoftwareRenderer::shutDown()
+{
     mCamera = nullptr;
-    if (startUpComplete){
+    if (startUpComplete)
+    {
         delete zBuffer;
         delete pixelBuffer;
     }
 }
 
-void SoftwareRenderer::drawTriangularMesh(Model * currentModel){
-    //Getting the vertices, faces, texture data 
+void SoftwareRenderer::drawTriangularMesh(Model* currentModel)
+{
+    // Getting the vertices, faces, texture data
 
-    Mesh *triMesh = currentModel->getMesh();
-    std::vector<Vector3i> * vIndices = &triMesh->vertexIndices;
-    std::vector<Vector3i> * tIndices = &triMesh->textureIndices;
-    std::vector<Vector3i> * nIndices = &triMesh->normalsIndices;
+    Mesh*                  triMesh  = currentModel->getMesh();
+    std::vector<Vector3i>* vIndices = &triMesh->vertexIndices;
+    std::vector<Vector3i>* tIndices = &triMesh->textureIndices;
+    std::vector<Vector3i>* nIndices = &triMesh->normalsIndices;
 
-    std::vector<Vector3f> * vertices = &triMesh->vertices;
-    std::vector<Vector3f> * normals  = &triMesh->normals;
-    std::vector<Vector3f> * fNormals = &triMesh->fNormals;
-    std::vector<Vector3f> * texels   = &triMesh->texels;
-    int numFaces = triMesh->numFaces;
+    std::vector<Vector3f>* vertices = &triMesh->vertices;
+    std::vector<Vector3f>* normals  = &triMesh->normals;
+    std::vector<Vector3f>* fNormals = &triMesh->fNormals;
+    std::vector<Vector3f>* texels   = &triMesh->texels;
+    int                    numFaces = triMesh->numFaces;
 
-    //Array grouping vertices together into triangle
+    // Array grouping vertices together into triangle
     Vector3f trianglePrimitive[3];
     Vector3f normalPrim[3];
     Vector3f uvPrim[3];
 
-    //Initializing shader 
+    // Initializing shader
     BlinnPhongShader shader;
     shader.albedo = currentModel->getAlbedo();
 
-    //Basic light direction
-    float t = static_cast<float>(SDL_GetTicks());
-    float radius = 1;
-    float lX   = std::sin(t/4000) * radius;
-    float lY   = std::cos(t/4000) * radius;
-    Vector3f lightDir{1, 0, 0};
+    // Basic light direction
+    float    t      = static_cast<float>(SDL_GetTicks());
+    float    radius = 1;
+    float    lX     = std::sin(t / 4000) * radius;
+    float    lY     = std::cos(t / 4000) * radius;
+    Vector3f lightDir {1, 0, 0};
     lightDir = lightDir.normalized();
 
-    //Building ModelViewProjection matrix
+    // Building ModelViewProjection matrix
     Matrix4 worldToObject = (*(currentModel->getModelMatrix())).inverse();
-    shader.MV  = (mCamera->viewMatrix)*(*(currentModel->getModelMatrix()));
-    shader.MVP = (mCamera->projectionMatrix)*shader.MV;
-    shader.V   = (mCamera->viewMatrix);
-    shader.N   = (shader.MV.inverse()).transpose(); 
+    shader.MV             = (mCamera->viewMatrix) * (*(currentModel->getModelMatrix()));
+    shader.MVP            = (mCamera->projectionMatrix) * shader.MV;
+    shader.V              = (mCamera->viewMatrix);
+    shader.N              = (shader.MV.inverse()).transpose();
 
-    //Iterate through every triangle
+    // Iterate through every triangle
     int count = 0;
-    for (int j = 0; j < numFaces; ++j){
-        //Current vertex and normal indices
+
+#pragma omp parallel for private(trianglePrimitive, normalPrim, uvPrim) firstprivate(shader)
+    for (int j = 0; j < numFaces; ++j)
+    {
+        // Current vertex and normal indices
         Vector3i f = (*vIndices)[j];
         Vector3i n = (*nIndices)[j];
         Vector3i u = (*tIndices)[j];
 
-        //Pack vertex, normal and UV data into arrays
-        buildTri(f,trianglePrimitive, *vertices);
-        buildTri(n,normalPrim, *normals);
-        buildTri(u,uvPrim, *texels);
+        // Pack vertex, normal and UV data into arrays
+        buildTri(f, trianglePrimitive, *vertices);
+        buildTri(n, normalPrim, *normals);
+        buildTri(u, uvPrim, *texels);
 
-        //Early quit if 
-        if (backFaceCulling((*fNormals)[j], trianglePrimitive[0], worldToObject)) continue;
+        // Early quit if
+        if (backFaceCulling((*fNormals)[j], trianglePrimitive[0], worldToObject))
+            continue;
         ++count;
-        //Apply vertex shader
-        for(int i = 0; i < 3; ++i){
-            trianglePrimitive[i] = shader.vertex(trianglePrimitive[i],
-                                                normalPrim[i],
-                                                uvPrim[i],
-                                                lightDir, i);
+        // Apply vertex shader
+        for (int i = 0; i < 3; ++i)
+        {
+            trianglePrimitive[i] = shader.vertex(trianglePrimitive[i], normalPrim[i], uvPrim[i], lightDir, i);
         }
 
-        //Skip triangles that are outside viewing frustrum
-        //Does not rebuild triangles that are partially out TO DO
-        if (clipTriangles(trianglePrimitive)) continue;
+        // Skip triangles that are outside viewing frustrum
+        // Does not rebuild triangles that are partially out TO DO
+        if (clipTriangles(trianglePrimitive))
+            continue;
 
         perspectiveDivide(trianglePrimitive);
 
-        //Send to rasterizer which will also call the fragment shader and write to the 
-        //zbuffer and pixel buffer.
+        // Send to rasterizer which will also call the fragment shader and write to the
+        // zbuffer and pixel buffer.
         Rasterizer::drawTriangles(trianglePrimitive, shader, pixelBuffer, zBuffer);
-        
     }
     printf("%d faces drawn.\n", count);
 }
 
-void SoftwareRenderer::clearBuffers(){
+void SoftwareRenderer::clearBuffers()
+{
     zBuffer->clear();
     pixelBuffer->clear();
 }
 
-Buffer<Uint32>* SoftwareRenderer::getRenderTarget(){
-    return pixelBuffer;
-}
+Buffer<Uint32>* SoftwareRenderer::getRenderTarget() { return pixelBuffer; }
 
-void SoftwareRenderer::setCameraToRenderFrom(Camera * camera){
-    mCamera = camera;
-}
+void SoftwareRenderer::setCameraToRenderFrom(Camera* camera) { mCamera = camera; }
 
-bool SoftwareRenderer::createBuffers(int w, int h){
-    int pixelCount = w*h;
-    bool success = true;
+bool SoftwareRenderer::createBuffers(int w, int h)
+{
+    int  pixelCount = w * h;
+    bool success    = true;
 
     zBuffer = new Buffer<float>(w, h, new float[pixelCount]);
-    if( zBuffer == nullptr){
+    if (zBuffer == nullptr)
+    {
         printf("Could not build z-Buffer.\n");
         success = false;
     }
-    else{
+    else
+    {
         pixelBuffer = new Buffer<Uint32>(w, h, new Uint32[pixelCount]);
-        if( pixelBuffer == nullptr){
+        if (pixelBuffer == nullptr)
+        {
             printf("Could not build pixel Buffer.\n");
             success = false;
         }
@@ -129,36 +138,43 @@ bool SoftwareRenderer::createBuffers(int w, int h){
     return success;
 }
 
-void SoftwareRenderer::buildTri(Vector3i &index, Vector3f *primitive, std::vector<Vector3f> &vals){
-    for(int i = 0; i < 3; ++i){
+void SoftwareRenderer::buildTri(Vector3i& index, Vector3f* primitive, std::vector<Vector3f>& vals)
+{
+    for (int i = 0; i < 3; ++i)
+    {
         primitive[i] = vals[index.data[i]];
     }
 }
 
-bool SoftwareRenderer::backFaceCulling(Vector3f &facetNormal, Vector3f &vert,  Matrix4 &worldToObject){
-        Vector3f viewDir =  worldToObject.matMultVec(mCamera->position) -  vert;
-        viewDir.normalized();
+bool SoftwareRenderer::backFaceCulling(Vector3f& facetNormal, Vector3f& vert, Matrix4& worldToObject)
+{
+    Vector3f viewDir = worldToObject.matMultVec(mCamera->position) - vert;
+    viewDir.normalized();
 
-        //Returns false if the triangle cannot see the camera
-        float intensity =  facetNormal.dotProduct(viewDir);
-        return intensity <= 0.0;
+    // Returns false if the triangle cannot see the camera
+    float intensity = facetNormal.dotProduct(viewDir);
+    return intensity <= 0.0;
 }
 
-bool SoftwareRenderer::clipTriangles(Vector3f *clipSpaceVertices){
+bool SoftwareRenderer::clipTriangles(Vector3f* clipSpaceVertices)
+{
     int count = 0;
-    for(int i = 0; i < 3; ++i){
+    for (int i = 0; i < 3; ++i)
+    {
         Vector3f vertex = clipSpaceVertices[i];
-        bool inside = (-vertex.w <= vertex.x <= vertex.w ) 
-                    && (-vertex.w <= vertex.y <= vertex.w)
-                    && (0 <= vertex.z <= vertex.w);
-        if (!inside) ++count;
+        bool     inside =
+            (-vertex.w <= vertex.x <= vertex.w) && (-vertex.w <= vertex.y <= vertex.w) && (0 <= vertex.z <= vertex.w);
+        if (!inside)
+            ++count;
     }
-    //If count equals three it means every vertex was out so we skip it
-    return count == 3 ;
+    // If count equals three it means every vertex was out so we skip it
+    return count == 3;
 }
 
-void SoftwareRenderer::perspectiveDivide(Vector3f *clippedVertices){
-    for(int i = 0; i < 3; ++i){
+void SoftwareRenderer::perspectiveDivide(Vector3f* clippedVertices)
+{
+    for (int i = 0; i < 3; ++i)
+    {
         clippedVertices[i].perspectiveDivide();
     }
 }
